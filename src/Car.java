f14bddb950073e0c8fd6efa28f80d4a347c0fef5
bharattanwar <bharat01rajput@gmail.com>
2024-04-06 19:11:32 +0530

    added car class

---
+++ src/Car.java
@@ -0,0 +1,25 @@
+public class Car {
+    private int price;
+    private int speed;
+
+    public Car(int price, int speed) {
+        this.price = price;
+        this.speed = speed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
